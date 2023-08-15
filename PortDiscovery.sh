2d25d1d6a86d3bb395738c4ef8f4c4ffc0a87e4c
chvck <137849899+chvckrocknrolla@users.noreply.github.com>
2023-08-15 14:36:26 +0000

    Create PortDiscovery.sh
    The script performs a port scanning within a specific range (1-65535) on a specific IP address (10.10.0.134). It utilizes the timeout command and input/output redirection to attempt sending an empty message to each port using Bash's /dev/tcp feature. If the port is open and responds, a message indicating that the port is open is displayed.

---
+++ PortDiscovery.sh
@@ -0,0 +1,40 @@
+#Port Discovery
+#             *     ,MMM8&&&.            *
+#                  MMMM88&&&&&    .
+#                 MMMM88&&&&&&&
+#     *           MMM88&&&&&&&&
+#                 MMM88&&&&&&&&
+#                 'MMM88&&&&&&'
+#                   'MMM8&&&'      *
+#          |\___/|
+#          )     (             .              '
+#         =\     /=
+#           )===(       *
+#          /     \
+#          |     |
+#         /       \
+#         \       /
+#  _/\_/\_/\__  _/_/\_/\_/\_/\_/\_/\_/\_/\_/\_
+#  |  |  |  |( (  |  |  |  |  |  |  |  |  |  |
+#  |  |  |  | ) ) |  |  |  |  |  |  |  |  |  |
+#  |  |  |  |(_(  |  |  |  |  |  |  |  |  |  |
+#  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |
+#     |  |  |  |  |  |  |  |  |  |  |  |  |  |
+#!/bin/bash
+
+function ctrl_c(){
+    echo -e "\n\n[!] Quit\n"
+    tput cnorm; exit 1
+}
+
+#ctrl+C
+trap ctrl_c INT
+
+tput civis
+
+for port in $(seq 1 65535); do
+    timeout 1 bash -c "echo '' > /dev/tcp/10.10.0.134/$port" 2>/dev/null && echo "Port $port - Open" &
+done
+wait
+
+tput cnorm
